e3b6ac380d3193d6250b918dd23677afee52b9f8
Alvaro Artavia <>
2024-10-07 11:15:27 -0600

    interface updated

--- MauiTestApp/Services/IAuthService.cs
+++ MauiTestApp/Services/IAuthService.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Results;
 
 namespace MauiTestApp.Services
 {
     public interface IAuthService
     {
         Task<LoginResult> LoginAsync();
+        Task<RefreshTokenResult> RefreshTokenAsync();
     }
 }
